8938480e7324fd71a0c0b66487ed6312c42b5c98
jayadityaroy <jayadityaroy09@gmail.com>
2026-06-15 00:06:33 +0530

    added variable.js file

---
+++ variable.js
@@ -0,0 +1,27 @@
+// variable: a container for storing data values
+// can create a variable using var, let, or const keywords
+// var: can be re-assigned and re-declared, has function scope, not recomended
+// let: can be re-assigned but not re-declared, has block scope
+// const: cannot be re-assigned or re-declared, has block scope
+
+// Datatypes:
+
+// Primitives:
+let name = "John"; // string
+let age = 30; // number
+let isStudent = true; // boolean
+let nullValue = null; // null: represents the intentional absence of any object value
+let undefinedValue; // undefined: represents the absence of a value
+
+// Reference:
+// object:
+let user={
+    name:"John",
+    age:30
+}
+// array: 
+let arr=[1,2,3,4,5];
+
+console.log(x);
+console.log(user);
+console.log(arr);
