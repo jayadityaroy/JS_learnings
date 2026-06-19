95279afcfbef862a001b87961d45454d4e10be71
jayadityaroy <jayadityaroy09@gmail.com>
2026-06-19 13:42:04 +0530

    added arrays.js

---
+++ arrays.js
@@ -0,0 +1,5 @@
+let numbers = [10, 20, 30, 40, 50];
+console.log(numbers);  // [10, 20, 30, 40, 50]
+
+let fruits = ["apple", "banana", "cherry"];
+console.log(fruits);  // ["apple", "banana", "cherry"]
