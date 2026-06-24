0415dcd3f49c0bfa8c2731aaa4c0eacb9e0d4ade
jayadityaroy <jayadityaroy09@gmail.com>
2026-06-25 01:00:18 +0530

    updated index.js from domExample

--- domExample/index.js
+++ domExample/index.js
@@ -1,9 +1,9 @@
 console.log("DOM Example");
 
-const heading = document.getElementById("magic-btn"); // Scan through the active DOM tree (of index.html), find the element that has the ID magic-btn
-console.log(heading);
+const btn = document.getElementById("magic-btn"); // Scan through the active DOM tree (of index.html), find the element that has the ID magic-btn
+console.log(btn);
 
-heading.onclick = function () {
+btn.onclick = function git() {
     console.log("Button clicked!");
-    document.getElementById("text").innerText = "Button was clicked!";
+    document.getElementById("text").innerText = "Button was clicked!"; // scan through the active DOM tree (of index.html), find the element that has the ID: text, and change its inner text to "Button was clicked!"
 }
